9f7178c6e8332395713fad0fad577ea4cc888a3e
Gokulakrishnan <Gokulakrishnan@LAPTOP-8QE8IR6F>
2024-12-19 11:52:39 +0530

    push1

--- Selenium_Basic/src/main/java/Selenium_Basic/Practice.java
+++ Selenium_Basic/src/main/java/Selenium_Basic/Practice.java
@@ -13,7 +13,7 @@ public class Practice {
 	public static void main(String[] args)
 	{
 		String s = "Hello";
-        for (int i = s.length() - 1; i >= 0; i--)
+        for (int i = s.length()-1; i >= 0; i--)
       { 
             System.out.println(s.charAt(i)); 
 	}
